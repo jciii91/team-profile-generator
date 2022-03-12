1ea9b1cea9d652199ddf77294fca8970391107a1
JCIII <jcostello711@gmail.com>
2022-03-12 14:23:36 -0500

    Employee passes 1st test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,7 @@
 const Employee = require('../lib/Employee');
 
 test('creates an Employee object', () => {
-    const employee = new Employee('John');
+    const employee = new Employee('John',1,'john@test.com');
 
     expect(employee.name).toBe('John');
     expect(employee.id).toEqual(expect.any(Number));
