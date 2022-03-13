3df9e0403c3816c49b229d49d93adcf53becb1ad
JCIII <jcostello711@gmail.com>
2022-03-12 19:03:39 -0500

    engineers can now be added to the team

--- index.js
+++ index.js
@@ -63,7 +63,84 @@ function getManager() {
 }
 
 function getEngineer() {
-
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the engineer\'s name? (Required)',
+            validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a name!');
+                return false;
+            }
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: 'What is the engineer\'s employee ID? (Required)',
+            validate: employeeID => {
+            if (employeeID) {
+                return true;
+            } else {
+                console.log('Please enter an ID!');
+                return false;
+            }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the engineer\'s email? (Required)',
+            validate: email => {
+            if (email) {
+                return true;
+            } else {
+                console.log('Please enter an email!');
+                return false;
+            }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the engineer\'s github username? (Required)',
+            validate: github => {
+            if (github) {
+                return true;
+            } else {
+                console.log('Please enter a username!');
+                return false;
+            }
+            }
+        },
+    ])
+    .then(engineer => {
+        const {name:name,employeeID:employeeID,email:email,github:github} = engineer;
+        tempEng = new Engineer(name,employeeID,email,github);
+        newTeam.push(tempEng);
+    })
+    .then(() => {
+        return inquirer.prompt(
+            {
+                type: 'list',
+                name: 'option',
+                message: 'What would you like to do now?',
+                choices: ['Add an engineer','Add an intern','Finish building your team']
+            }
+        )
+    })
+    .then(option => {
+        if (option.option == 'Add an engineer') {
+            return getEngineer();
+        } else if (option.option == 'Add an intern') {
+            return 'I';
+        } else {
+            return 'F';
+        }
+    })
 }
 
 function buildTeam(manager) {
@@ -96,6 +173,6 @@ getManager()
     .then(manager => {
         return buildTeam(manager);
     })
-    .then (answers => {
-        console.log(answers);
+    .then (() => {
+        console.log(newTeam);
     });
